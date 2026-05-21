7ffc751fdde049580c60ca444d0a2a8d81f208b3
Alex Khitry <khitrii03@gmail.com>
2026-05-21 16:16:19 +0300

    feat: add husky

--- apps/profile-service/src/main.ts
+++ apps/profile-service/src/main.ts
@@ -1,18 +1,14 @@
-import Fastify from 'fastify';
-import { app } from './app/app';
+import Fastify from "fastify";
+import { app } from "./app/app";
+import { loggerInstance } from "@org/shared";
 
-const host = process.env.HOST ?? 'localhost';
+const host = process.env.HOST ?? "localhost";
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
-// Instantiate Fastify with some config
-const server = Fastify({
-  logger: true,
-});
+const server = Fastify({ loggerInstance });
 
-// Register your application as a normal plugin.
 server.register(app);
 
-// Start listening.
 server.listen({ port, host }, (err) => {
   if (err) {
     server.log.error(err);
